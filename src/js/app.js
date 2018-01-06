13f1456d79a0dbed86e3f35c842b5083630c4bd1
xie-hui <polarcracker@gmail.com>
2018-01-05 21:59:38 -0800

    create gh-pages

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/app-main'
-
-import '../styles/main.scss'; // Import CSS
-
-ReactDOM.render( < App / > , document.getElementById('app'));
